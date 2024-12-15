6bb2ce1b915950bbe9d33d6823f64062e9851077
vonssy <kak.reyvaldizakaria@gmail.com>
2024-12-15 16:44:51 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Dawn Validator BOT
+Dawn Validator BOT
+
+Download Extension Here : [Dawn Validator](https://chromewebstore.google.com/detail/dawn-validator-chrome-ext/fpdkjdnhkakefebpekbdhillbhonfjjp?hl=en)
+Use Code : 02lt4r
+
+## Fitur
+
+  - Auto Get Account Information
+  - Auto Use [Monosans Proxy](https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt)
+  - Auto Renew Proxy
+  - Auto Send Keep-Alive
+  - Multi Account
+
+## Prasyarat
+
+Pastikan Anda telah menginstal Python3.9 dan PIP.
+
+## Instalasi
+
+1. **Kloning repositori:**
+   ```bash
+   git clone https://github.com/vonssy/Dawn-BOT.git
+   ```
+   ```bash
+   cd Dawn-BOT
+   ```
+
+2. **Instal Requirements:**
+   ```bash
+   pip install -r requirements.txt #or pip3 install -r requirements.txt
+   ```
+
+## Konfigurasi
+
+- **accounts.json:** Anda akan menemukan file `accounts.json` di dalam direktori proyek. Pastikan `accounts.json` berisi data yang sesuai dengan format yang diharapkan oleh skrip. Berikut adalah contoh format file:
+
+  ```bash
+    [
+        {
+            "Email": "your_email_address 1",
+            "Token": "your_berear_token 1"
+        },
+        {
+            "Email": "your_email_address 2",
+            "Token": "your_berear_token 2"
+        }
+    ]
+  ```
+
+## Jalankan
+
+```bash
+python bot.py #or python3 bot.py
+```
+
+## Penutup
+
+Terima kasih telah mengunjungi repository ini, jangan lupa untuk memberikan kontribusi berupa follow dan stars.
+Jika Anda memiliki pertanyaan, menemukan masalah, atau memiliki saran untuk perbaikan, jangan ragu untuk menghubungi saya atau membuka *issue* di repositori GitHub ini.
+
+**vonssy**
---
+++ bot.py
@@ -0,0 +1,288 @@
+from aiohttp import (
+    ClientResponseError,
+    ClientSession,
+    ClientTimeout
+)
+from aiohttp_socks import ProxyConnector
+from colorama import *
+from datetime import datetime
+from fake_useragent import FakeUserAgent
+import asyncio, json, os, pytz, uuid
+
+wib = pytz.timezone('Asia/Jakarta')
+
+class Dawn:
+    def __init__(self) -> None:
+        self.headers = {
+            "Accept": "*/*",
+            "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Cache-Control": "no-cache",
+            "Host": "www.aeropres.in",
+            "Origin": "chrome-extension://fpdkjdnhkakefebpekbdhillbhonfjjp",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "cross-site",
+            "User-Agent": FakeUserAgent().random
+        }
+        self.extension_id = "fpdkjdnhkakefebpekbdhillbhonfjjp"
+        self.proxies = []
+        self.proxy_index = 0
+
+    def clear_terminal(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def log(self, message):
+        print(
+            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}{message}",
+            flush=True
+        )
+
+    def welcome(self):
+        print(
+            f"""
+        {Fore.GREEN + Style.BRIGHT}Auto Ping {Fore.BLUE + Style.BRIGHT}Dawn - BOT
+            """
+            f"""
+        {Fore.GREEN + Style.BRIGHT}Rey? {Fore.YELLOW + Style.BRIGHT}<INI WATERMARK>
+            """
+        )
+
+    def format_seconds(self, seconds):
+        hours, remainder = divmod(seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
+    
+    async def load_proxies(self):
+        url = "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt"
+        try:
+            async with ClientSession(timeout=ClientTimeout(total=20)) as session:
+                async with session.get(url=url) as response:
+                    response.raise_for_status()
+                    content = await response.text()
+                    with open('proxy.txt', 'w') as f:
+                        f.write(content)
+
+                    self.proxies = content.splitlines()
+                    print(f"{Fore.GREEN}Proxies successfully downloaded.{Style.RESET_ALL}")
+                    print(f"{Fore.YELLOW}Loaded {len(self.proxies)} proxies.{Style.RESET_ALL}")
+                    await asyncio.sleep(3)
+        except Exception as e:
+            print(f"{Fore.RED}Failed to load proxies: {e}{Style.RESET_ALL}")
+            return []
+
+    def get_next_proxy(self):
+        if not self.proxies:
+            return None
+
+        proxy = self.proxies[self.proxy_index]
+        self.proxy_index = (self.proxy_index + 1) % len(self.proxies)
+        return proxy
+    
+    def check_proxy_schemes(self, proxies):
+        schemes = ["http://", "https://", "socks4://", "socks5://"]
+        if any(proxies.startswith(scheme) for scheme in schemes):
+            return proxies
+        
+        return f"socks5://{proxies}"
+
+    def load_accounts(self):
+        try:
+            if not os.path.exists('accounts.json'):
+                self.log(f"{Fore.RED}File 'accounts.json' tidak ditemukan.{Style.RESET_ALL}")
+                return
+
+            with open('accounts.json', 'r') as file:
+                data = json.load(file)
+                if isinstance(data, list):
+                    return data
+                return []
+        except json.JSONDecodeError:
+            return []
+            
+    def generate_app_id(self):
+        return uuid.uuid4().hex
+    
+    def hide_email(self, email):
+        local, domain = email.split('@', 1)
+        hide_local = local[:3] + '*' * 3 + local[-3:]
+        return f"{hide_local}@{domain}"
+        
+    async def user_data(self, app_id: str, token: str, proxy=None):
+        url = f"https://www.aeropres.in/api/atom/v1/userreferral/getpoint?appid={app_id}"
+        headers = {
+            **self.headers,
+            "Authorization": f"Berear {token}",
+            "Content-Type": "application/json",
+        }
+        if proxy:
+            connector = ProxyConnector.from_url(proxy)
+        try:
+            async with ClientSession(connector=connector, timeout=ClientTimeout(total=20)) as session:
+                async with session.get(url=url, headers=headers) as response:
+                    response.raise_for_status()
+                    result = await response.json()
+                    return result['data']['rewardPoint']
+        except (Exception, ClientResponseError) as e:
+            return None
+        
+    async def send_keepalive(self, app_id: str, token: str, email: str, proxy=None, retries=60):
+        url = f"https://www.aeropres.in/chromeapi/dawn/v1/userreward/keepalive?appid={app_id}"
+        data = json.dumps({"username":email, "extensionid":self.extension_id, "numberoftabs":0, "_v":"1.1.1"})
+        headers = {
+            **self.headers,
+            "Authorization": f"Berear {token}",
+            "Content-Length": str(len(data)),
+            "Content-Type": "application/json",
+        }
+        for attempt in range(retries):
+            if proxy:
+                connector = ProxyConnector.from_url(proxy)
+            try:
+                async with ClientSession(connector=connector, timeout=ClientTimeout(total=10)) as session:
+                    async with session.post(url=url, headers=headers, data=data) as response:
+                        response.raise_for_status()
+                        return await response.json()
+            except (Exception, ClientResponseError) as e:
+                if attempt < retries - 1:
+                    continue
+                return None
+        
+    async def process_accounts(self, app_id: str, token: str, email: str):
+        user = None
+        proxies = self.get_next_proxy()
+        proxy = self.check_proxy_schemes(proxies)
+        hide_email = self.hide_email(email)
+        
+        while user is None:
+            user = await self.user_data(app_id, token, proxy)
+            
+            if not user:
+                self.log(
+                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
+                    f"{Fore.RED + Style.BRIGHT}Login Failed{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} With Proxy {proxy} {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                )
+                await asyncio.sleep(1)
+            
+                print(
+                    f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                    f"{Fore.BLUE + Style.BRIGHT}Try With The Next Proxy,{Style.RESET_ALL}"
+                    f"{Fore.YELLOW + Style.BRIGHT} Wait... {Style.RESET_ALL}",
+                    end="\r",
+                    flush=True
+                )
+                
+                proxies = self.get_next_proxy()
+                proxy = self.check_proxy_schemes(proxies)
+                continue
+
+        total_points = sum(value for key, value in user.items() if 'points' in key and isinstance(value, (int, float)))
+        self.log(
+            f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
+            f"{Fore.GREEN + Style.BRIGHT}Login Success{Style.RESET_ALL}"
+            f"{Fore.MAGENTA + Style.BRIGHT} ] [ Balance{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} {total_points:.0f} Points {Style.RESET_ALL}"
+            f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+        )
+        await asyncio.sleep(1)
+
+        print(
+            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+            f"{Fore.BLUE + Style.BRIGHT}Try to Sent Ping,{Style.RESET_ALL}"
+            f"{Fore.YELLOW + Style.BRIGHT} Wait... {Style.RESET_ALL}",
+            end="\r",
+            flush=True
+        )
+        await asyncio.sleep(1)
+
+        keepalive = await self.send_keepalive(app_id, token, email, proxy)
+        if not keepalive:
+            self.log(
+                f"{Fore.MAGENTA + Style.BRIGHT}[ Ping{Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT} Sent With Proxy {proxy} {Style.RESET_ALL}"
+                f"{Fore.MAGENTA + Style.BRIGHT}] [ Status{Style.RESET_ALL}"
+                f"{Fore.YELLOW + Style.BRIGHT} Keep Alive Not Recorded {Style.RESET_ALL}"
+                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+            )
+            return
+
+        if keepalive:
+            self.log(
+                f"{Fore.MAGENTA + Style.BRIGHT}[ Ping{Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT} Sent With Proxy {proxy} {Style.RESET_ALL}"
+                f"{Fore.MAGENTA + Style.BRIGHT}] [ Status{Style.RESET_ALL}"
+                f"{Fore.GREEN + Style.BRIGHT} Keep Alive Recorded {Style.RESET_ALL}"
+                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+            )
+    
+    async def main(self):
+        try:
+            accounts = self.load_accounts()
+            if not accounts:
+                self.log(f"{Fore.RED}No accounts loaded from 'accounts.json'.{Style.RESET_ALL}")
+                return
+            
+            last_proxy_update = None
+            proxy_update_interval = 1800
+
+            while True:
+                if not last_proxy_update or (datetime.now() - last_proxy_update).total_seconds() > proxy_update_interval:
+                    await self.load_proxies()
+                    last_proxy_update = datetime.now()
+
+                self.clear_terminal()
+                self.welcome()
+                self.log(
+                    f"{Fore.GREEN + Style.BRIGHT}Account's Total: {Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT}{len(accounts)}{Style.RESET_ALL}"
+                )
+                self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
+
+                for account in accounts:
+                    app_id = self.generate_app_id()
+                    token = account.get('Token')
+                    email = account.get('Email', 'Unknown Email')
+
+                    if not token:
+                        self.log(
+                            f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                            f"{Fore.WHITE + Style.BRIGHT} {email} {Style.RESET_ALL}"
+                            f"{Fore.YELLOW + Style.BRIGHT}Token Not Found in 'accounts.json'{Style.RESET_ALL}"
+                            f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                        )
+                        continue
+
+                    await self.process_accounts(app_id, token, email)
+                    self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
+
+                seconds = 120
+                while seconds > 0:
+                    formatted_time = self.format_seconds(seconds)
+                    print(
+                        f"{Fore.CYAN+Style.BRIGHT}[ Wait for{Style.RESET_ALL}"
+                        f"{Fore.WHITE+Style.BRIGHT} {formatted_time} {Style.RESET_ALL}"
+                        f"{Fore.CYAN+Style.BRIGHT}... ]{Style.RESET_ALL}",
+                        end="\r"
+                    )
+                    await asyncio.sleep(1)
+                    seconds -= 1
+
+        except Exception as e:
+            self.log(f"{Fore.RED+Style.BRIGHT}Error: {e}{Style.RESET_ALL}")
+
+if __name__ == "__main__":
+    try:
+        bot = Dawn()
+        asyncio.run(bot.main())
+    except KeyboardInterrupt:
+        print(
+            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+            f"{Fore.RED + Style.BRIGHT}[ EXIT ] Dawn - BOT{Style.RESET_ALL}",                                       
+        )
